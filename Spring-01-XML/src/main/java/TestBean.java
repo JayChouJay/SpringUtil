e5bee5006278750931888eaf8bbadf5ddae3949e
JayChouJay <1020968571@qq.com>
2024-02-25 22:40:37 +0800

    第er次提交

--- Spring-01-XML/src/main/java/TestBean.java
+++ Spring-01-XML/src/main/java/TestBean.java
@@ -7,9 +7,10 @@ public class TestBean {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext factory = new ClassPathXmlApplicationContext("spring/ApplicationContext.xml");
 
+
         //国际化测试失败
-//        MessageSource ms = (MessageSource) factory.getBean("messageSource");
-//        String name = ms.getMessage("name", null, "def", new Locale("zh", "CN"));
-//        System.out.println(name);
+       // MessageSource ms = (MessageSource) factory.getBean("messageSource");
+       // String name = ms.getMessage("name", null, "def", new Locale("zh", "CN"));
+       // System.out.println(name);
     }
 }
